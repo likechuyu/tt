86495fac9899660168b04f4753844e7171604d0b
cyyn <1272581164@qq.com>
2019-10-26 11:09:17 +0800

    “登陆页面绘制完成

---
+++ src/views/login/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <el-card class="box-card">
+      <img src="../../assets/logo_index.png" alt />
+      <el-form>
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            style="width:240px;margin-right:8px;"
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%;" @click="onSubmit" type="primary">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: { mobile: '', code: '' }
+    }
+  },
+  methods: {
+    onSubmit () { }
+  }
+}
+</script>
+<style lang="less" scoped>
+.container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  .el-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+    }
+  }
+}
+</style>
